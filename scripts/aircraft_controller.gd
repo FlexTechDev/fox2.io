dbbb2018b1c54b2fdce508dfe5f4ac34c1e70556
x32dev <120539187+FlexTechDev@users.noreply.github.com>
2023-11-18 10:56:24 -0500

    input changes and water effects

--- scripts/aircraft_controller.gd
+++ scripts/aircraft_controller.gd
@@ -2,6 +2,10 @@ extends CharacterBody3D
 
 @export var settings: AircraftSettings
 
+@onready var water_stream_node: Node3D = get_node("../WaterStreamEffect");
+@onready var ship: Node3D = $ship;
+@onready var raycast: RayCast3D = $RayCast3D;
+
 var forward_speed: float = 0;
 var target_speed: float = 0;
 
@@ -10,14 +14,19 @@ var pitch_input: float = 0;
 var smoothed_turn_input: float = 0;
 var smoothed_pitch_input: float = 0;
 
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED;
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		pitch_input = event.relative.y / 25;
+		turn_input = -event.relative.x / 25;
+
 func get_input(delta: float) -> void:
 	if Input.is_action_pressed("throttle_up"):
 		target_speed = min(forward_speed + settings.throttle_delta * delta, settings.max_speed);
 	elif Input.is_action_pressed("throttle_down"):
 		target_speed = max(forward_speed - settings.throttle_delta * delta, settings.min_speed);
-	
-	turn_input = Input.get_axis("roll_right", "roll_left");
-	pitch_input = Input.get_axis("pitch_up", "pitch_down");
 	
 	smoothed_turn_input = lerp(smoothed_turn_input, turn_input, settings.lerp_speed * delta);
 	smoothed_pitch_input = lerp(smoothed_pitch_input, pitch_input, settings.lerp_speed * delta);
@@ -31,13 +40,21 @@ func _physics_process(delta: float) -> void:
 	transform.basis = transform.basis.rotated(transform.basis.x, smoothed_pitch_input * settings.pitch_speed * delta);
 	transform.basis = transform.basis.rotated(up_vector.normalized(), smoothed_turn_input * settings.turn_speed * delta);
 	
-	$ship.rotation.z = lerp($ship.rotation.z, -turn_input / settings.roll_lean_divisor, settings.level_speed * delta);
-	$ship.rotation.x = lerp($ship.rotation.x, pitch_input / settings.pitch_lean_divisor, settings.level_speed * delta);
-	$ship.rotation.y = lerp($ship.rotation.y, turn_input / settings.turn_lean_divisor, settings.level_speed * delta);
+	ship.rotation.z = lerp(ship.rotation.z, -turn_input / settings.roll_lean_divisor, settings.level_speed * delta);
+	ship.rotation.x = lerp(ship.rotation.x, pitch_input / settings.pitch_lean_divisor, settings.level_speed * delta);
+	ship.rotation.y = lerp(ship.rotation.y, turn_input / settings.turn_lean_divisor, settings.level_speed * delta);
 	
 	forward_speed = lerp(forward_speed, target_speed, settings.acceleration * delta);
 	velocity = transform.basis.z * forward_speed;
 	
 	move_and_slide();
 	
-	$RayCast3D.global_rotation = Vector3.ZERO;
+	raycast.global_rotation = Vector3.ZERO;
+	
+	if raycast.is_colliding():
+		var distance: float = 1 - (raycast.get_collision_point().distance_to(raycast.global_position) / abs(raycast.target_position.y));
+		water_stream_node.scale.y = distance * distance;
+		water_stream_node.global_position = raycast.get_collision_point();
+		water_stream_node.rotation.y = ship.global_rotation.y;
+	else:
+		water_stream_node.scale.y = 0;
